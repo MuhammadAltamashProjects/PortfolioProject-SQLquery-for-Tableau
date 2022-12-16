8b92bf05dee38513d3d496da0b91e40bf8c44076
MuhammadAltamashProjects <120510798+MuhammadAltamashProjects@users.noreply.github.com>
2022-12-16 17:30:56 +0500

    Add files via upload

---
+++ SQLQueryPortfolioProject for Tableau.sql
@@ -0,0 +1,66 @@
+/*
+Queries used for Tableau Project
+*/
+
+
+
+-- 1. 
+--To show global death percentage due to covid
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
+
+
+--Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+--From PortfolioProject..CovidDeaths
+----Where location like '%states%'
+--where 
+-- continent is  null
+----Group By date
+--order by 1,2
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+--To show a bar graph of total deaths in each continent
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+-- Using maxinfectedcases to figure out how much percent population is affected in total
+--To create a map with death percentage of each country
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount, 
+Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by location, population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+--To create line graph for specific countries for good visualization
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
